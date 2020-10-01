a5fd698b3a1841f3b6e31ad8aa965a77ac26e474
ip11 <iprash011@gmail.com>
2020-10-01 13:51:44 +0530

    Implemeted RESTful APIs

--- config.js
+++ config.js
@@ -1,22 +1,9 @@
-const environments = {};
+const config = {};
 
 
-environments.production = {
-    'httpPort' : 5000,
-    'envName' : 'production',
-    'hashingSecret' : 'theHackingSchool'
-}
-
-environments.dev = {
-'httpPort' : 3000,
-'envName' : 'development',
-'hashingSecret' : 'theHackingSchool'
+config.keys = {
+    'hashingSecret': 'theHackingSchool'
 }
 
 
-const currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
-
-const environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.dev;
-
-
-module.exports = environmentToExport;
+module.exports = config;
--- index.js
+++ index.js
@@ -2,70 +2,54 @@ const http = require('http');
 const url = require('url');
 const config = require('./config');
 const stringDecoder = require('string_decoder').StringDecoder;
-const port = config.httpPort;
-const fs = require('fs');
-const _data = require('./lib/data');
+const port = process.env.PORT || 3000;
 const handlers = require('./lib/handlers');
 const helpers = require("./lib/helpers");
 
-// _data.create('test', 'newFile', {'foo':'bar'},(err)=>{
-// console.log('This is an error : ',err);
-// });
 
-// _data.read('test', 'newFile', (err,data)=>{
-//     console.log('This is an error : ',err,data);
-// });
-    
-// _data.update('test','newFile', {'akhil':'teja'},(err)=>{
-//     console.log('There is an error : ',err);
-// })
+const server = http.createServer((req, res) => {
+    const parsedURL = url.parse(req.url, true);
+    const path = parsedURL.pathname;
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedURL.query;
+    const headers = req.headers;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const decoder = new stringDecoder('utf-8');
+    let buffer = '';
 
-// _data.delete('test','newFile',(err,data)=>{
-//     console.log('this is an error: ',err);
-// })
-
-const server = http.createServer((req,res)=>{
-  const parsedURL = url.parse(req.url, true);
-  const path = parsedURL.pathname;
-  const method = req.method.toLowerCase();
-  const queryStringObject = parsedURL.query;
-  const headers = req.headers;
-  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
-  const decoder = new stringDecoder('utf-8');
-  let buffer = '';
-
-    req.on('data', (data)=>{
+    req.on('data', (data) => {
         buffer += decoder.write(data);
     });
-    req.on('end',()=>{
+    req.on('end', () => {
         buffer += decoder.end();
-        const chosenHandler = typeof(router[trimmedPath])!== 'undefined' ? router[trimmedPath] : handlers.notFound;
+        const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
         const data = {
-            "trimmedPath":trimmedPath,
-            "queryStringObject" : queryStringObject,
-            "method" : method,
-            "headers" : headers,
-            "payload" : helpers.parseJsonToObject(buffer)
+            "trimmedPath": trimmedPath,
+            "queryStringObject": queryStringObject,
+            "method": method,
+            "headers": headers,
+            "payload": helpers.parseJsonToObject(buffer)
         }
-        chosenHandler(data, (statusCode, payload)=>{
-            statusCode = typeof(statusCode == 'number') ? statusCode :200;
-            payload = typeof(payload == 'object') ? payload : {};
+        chosenHandler(data, (statusCode, payload) => {
+            statusCode = typeof (statusCode == 'number') ? statusCode : 200;
+            payload = typeof (payload == 'object') ? payload : {};
             payloadString = JSON.stringify(payload);
             res.setHeader('Content-Type', 'application/json');
-                res.writeHead(statusCode);
-                res.end(payloadString);
-                console.log(statusCode,payloadString);
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log(statusCode, payloadString);
         });
-           
+
     });
 
 });
 
-server.listen(port, ()=>{
-    console.log(`Server Started on ${port} in ${config.envName} mode`);
+server.listen(port, () => {
+    console.log(`Server Started on ${port}`);
 });
 
 
-const router  = {
-    "users" : handlers.users
+const router = {
+    "users": handlers.users,
+    "/": handlers.home
 }
